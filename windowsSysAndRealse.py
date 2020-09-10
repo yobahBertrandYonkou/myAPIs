7d8902b4252a8096da160944cf24476b228b74e6
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2020-09-11 00:29:20 +0530

    windows os release and system info

---
+++ windowsSysAndRealse.py
@@ -0,0 +1,4 @@
+#!C:/Users/YOBAH BERTRAND Y/AppData/Local/Programs/Python/Python37/python.exe
+print("content-type: text/html\n")
+import platform
+print(platform.system() + " " + platform.release())
