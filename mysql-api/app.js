3217812126b75f7a97073474d20e72256daa2052
yobahBertrandYonkou <62288787+yobahBertrandYonkou@users.noreply.github.com>
2021-01-29 03:44:18 +0530

    A login API has been included include
    A login API has been included include
    - database: /database/?sql=<query>
    - login: /database/?password=<password>&email=<email>
    
    Login in API takes in password and email, queries the `users` table, and returns the record if found.

--- mysql-api/app.js
+++ mysql-api/app.js
@@ -3,59 +3,92 @@ var mysqldb = require('./db-connection')
 var url = require('url')
 var express =  require('express')
 var cors = require('cors')
+const { type } = require('os')
 
 //initializing express
 const app = express()
 
 //enabling cors
-app.use(cors())
+app.use(cors());
 
-//receiving, processing and responding requests
-app.get('/',(req, res)=>{
+//query executor
+
+var queryExecutor = (query, res) => {
+    //executing query
+    mysqldb.query(query, (error, result)=>{
+        if(error){
+            //formatting error
+            var err = {
+                code: error.code,
+                errno: error.errno,
+                sqlMessage: error.sqlMessage,
+                sqlSate: error.sqlState,
+                index: error.index,
+                sql: error.sql
+            }
+
+            //printing error
+            console.error(err);
+
+            //returning error message to client
+            //res.writeHead(200, {"content-type": "text/html"})
+            res.send(err.sqlMessage)
+        }else{
+            //prints result
+            console.log(result)
+
+            //returning data to client
+            //res.writeHead(200, {"content-type": "text/html"})
+            res.send(JSON.stringify(result))
+        }
+    });
+}
+
+//receiving, processing and responding to requests related to login
+app.get('/login', (req, res)=>{
+    //geting users credentials
+    var credentails = req.query;
+
+    //extracts password and email from query
+    var email = credentails['email'];
+    var password = credentails['password'];
+
+    //login query
+    var query = `select * from users where email='${email}' and password='${password}'`;
+
+    //executing query
+    queryExecutor(query, res);
+
+    
+});
+
+//receiving, processing and responding to requests related to database
+app.get('/database',(req, res)=>{
+
+        //url.parse is deprecated
+        // getting query string from request
+        // var query = url.parse(req.url).query
+
         //getting query string from request
-        var query = url.parse(req.url).query
+        var query = req.query;
 
-        //checks whether query string is null
-        if(query == null){
+        console.log(typeof(query))
+
+        //checks whether query string is not present or query string is null
+        if(Object.keys(query).length == 0 || query['sql'] == ""){
             console.log("\nQuery string is null\n")
         }else{
-            //processes query string  to get actual query
-            query = query.replace(/%20/gi, " ")
-                         .replace("sql=", "")
-                         .replace(/%27/gi, "\'")
-                         .replace(/%22/gi, "\"")
+            // processes query string  to get actual query
+            // query = query.replace(/%20/gi, " ")
+            //              .replace("sql=", "")
+            //              .replace(/%27/gi, "\'")
+            //              .replace(/%22/gi, "\"")
     
             //printing query to console
-            console.log(`\nSQL: ${query}`)
+            console.log(`\nSQL: ${query["sql"]}`)
     
-            //executing query
-            mysqldb.query(query, (error, result)=>{
-                if(error){
-                    //formatting error
-                    var err = {
-                        code: error.code,
-                        errno: error.errno,
-                        sqlMessage: error.sqlMessage,
-                        sqlSate: error.sqlState,
-                        index: error.index,
-                        sql: error.sql
-                    }
-    
-                    //printing error
-                    console.error(err);
-    
-                    //returning error message to client
-                    //res.writeHead(200, {"content-type": "text/html"})
-                    res.send(err.sqlMessage)
-                }else{
-                    //prints result
-                    console.log(result)
-    
-                    //returning data to client
-                    //res.writeHead(200, {"content-type": "text/html"})
-                    res.send(JSON.stringify(result))
-                }
-            })
+            //executes query and reply back to client
+            queryExecutor(query, res);
         }
 })
 //create server
